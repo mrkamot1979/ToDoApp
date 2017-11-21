83ea8b80aced74df7f09f60632034ce646cbfed3
nenu <nino_roldan@yahoo.com>
2017-11-21 19:50:34 +0900

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Nino Rorudan on 11/21/17.
+//  Copyright © 2017 The NTMC Foundation. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContexts
+        
+        
+        
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
