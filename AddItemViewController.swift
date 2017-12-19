f9b3d5fbda7768fc49884825139abaaf2f25b32b
nenu <nino_roldan@yahoo.com>
2017-12-19 20:20:01 +0900

    New AddItemViewcontroller

---
+++ AddItemViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AddItemViewController.swift
+//  ToDo
+//
+//  Created by Nino Rorudan on 12/19/17.
+//  Copyright © 2017 The NTMC Foundation. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var txtTitle: UITextField!
+    
+    
+    @IBOutlet weak var txtSubtitle: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
